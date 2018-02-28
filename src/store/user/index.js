527199ffe7a9799938af8456e8b87aab6b4d4207
cleverapollo <cleverapollo@gmail.com>
2018-02-28 15:10:07 +0000

    Add environment

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/api/'
+const API_URL = process.env.API_URL
 const SIGNUP_URL = `${API_URL}register`
 const SIGNIN_URL = `${API_URL}login`
 const SIGNOUT_URL = `${API_URL}logout`
