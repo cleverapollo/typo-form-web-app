12d0a15ce5f9540ebe9465d2d581473f4240c429
cleverapollo <cleverapollo@gmail.com>
2018-03-03 16:34:00 +0000

    Convert to Table

--- src/components/Team/Teams.vue
+++ src/components/Team/Teams.vue
@@ -1,41 +1,35 @@
 <template>
   <v-container>
-    <v-layout row wrap v-for="team in teams" :key="team.id" class="mb-2">
-      <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
-        <v-card class="info">
-          <v-container fluid>
-            <v-layout row>
-              <v-flex xs7 sm8 md9>
-                <v-card-title primary-title>
-                  <div>
-                    <h5 class="white--text mb-0">{{ team.name }}</h5>
-                  </div>
-                </v-card-title>
-                <v-card-text>
-                  <div>{{ team.description }}</div>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn flat @click=onLoadTeam(team.id)>
-                    <v-icon left light>arrow_forward</v-icon>
-                    View Team
-                  </v-btn>
-                </v-card-actions>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12>
+        <v-btn router to="/teams/new" class="primary">New Team</v-btn>
+        <v-data-table
+          :headers="headers"
+          :items="teams"
+          hide-actions
+          class="elevation-1"
+        >
+          <template slot="items" slot-scope="props">
+            <td @click=onLoadTeam(props.item.id)>{{ props.item.name }}</td>
+            <td @click=onLoadTeam(props.item.id)>Description</td>
+          </template>
+        </v-data-table>
       </v-flex>
     </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 sm10 md8 offset-sm1 offset-md2 text-xs-right>
-        <v-btn large router to="/teams/new" class="primary">Create New Team</v-btn>
-      </v-flex>
-    </v-layout>
+    
   </v-container>
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        headers: [
+          { text: 'Name', value: 'name', sortable: false, align: 'left' },
+          { text: 'Description', value: 'description', sortable: false, align: 'left' }
+        ]
+      }
+    },
     computed: {
       teams () {
         return this.$store.getters.loadedTeams
