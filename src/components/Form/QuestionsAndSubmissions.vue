d08ed5a4163d27f56827e7d78f27116ee3e9bcc4
cleverapollo <cleverapollo@gmail.com>
2018-03-28 07:06:12 +0800

    remove test text

--- src/components/Form/QuestionsAndSubmissions.vue
+++ src/components/Form/QuestionsAndSubmissions.vue
@@ -50,7 +50,6 @@
       return {
         tabs: ['Questions', 'Submissions'],
         active: null,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
       }
     },
     computed: {
