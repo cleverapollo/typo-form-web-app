50ab9940b10067048529b651ee43432d0a15d014
cleverapollo <cleverapollo@gmail.com>
2018-04-09 05:39:18 +0800

    linearscale components

--- src/components/Submission/Answer/components/LinearScale.vue
+++ src/components/Submission/Answer/components/LinearScale.vue
@@ -3,22 +3,23 @@
     <v-layout row class="p-2">
       <v-flex xs8 offset-xs2>
         <v-layout row>
-          <v-flex v-for="n in end" :key="n" v-if="n-1!=start">
+          <v-flex v-for="n in end.id" :key="n" v-if="n+1!=start.id">
             <span class="optionLabel">{{ n }}</span>
           </v-flex>
         </v-layout>
       </v-flex>
     </v-layout>
     <v-layout row style='p-2'>
-      <v-flex xs2 class="text-center">{{ startString }}</v-flex>
+      <v-flex xs2 class="text-center">{{ start.title }}</v-flex>
       <v-flex xs8>
         <v-layout row>
-          <v-radio-group row>
-            <v-radio v-for="n in end" :key="n" v-if="n-1!=start" :value="'radio-'+n"></v-radio>
+          <v-radio-group v-model="optionModel" row>
+            <v-radio v-for="n in end.id" :key="n" v-if="n+1!=start.id" :value="checked(n)"
+                     @change="onSave(n)"></v-radio>
           </v-radio-group>
         </v-layout>
       </v-flex>
-      <v-flex xs2 class="text-center">{{ endString }}</v-flex>
+      <v-flex xs2 class="text-center">{{ end.title }}</v-flex>
     </v-layout>
   </div>
 </template>
@@ -26,60 +27,57 @@
 <script>
   export default {
     name: 'linear-scale',
+    props: ['answers', 'responses'],
     data () {
       return {
-        startString: 'start',
-        endString: 'end',
-        start: 1,
-        end: 8,
-        startItems: [
-          {
-            id: 0,
-            text: '0'
-          },
-          {
+        optionModel: 'checked'
+      }
+    },
+    computed: {
+      start () {
+        if (this.answers.length < 2) {
+          return {
             id: 1,
-            text: '1'
+            title: ''
           }
-        ],
-        endItems: [
-          {
-            id: 2,
-            text: '2'
-          },
-          {
-            id: 3,
-            text: '3'
-          },
-          {
-            id: 4,
-            text: '4'
-          },
-          {
+        }
+        const start = this.answers[0].answer.substr(12)
+        const index = start.lastIndexOf('-')
+        return {
+          id: parseInt(start.substr(index + 1)),
+          title: start.substr(0, index)
+        }
+      },
+      end () {
+        if (this.answers.length < 2) {
+          return {
             id: 5,
-            text: '5'
-          },
-          {
-            id: 6,
-            text: '6'
-          },
-          {
-            id: 7,
-            text: '7'
-          },
-          {
-            id: 8,
-            text: '8'
-          },
-          {
-            id: 9,
-            text: '9'
-          },
-          {
-            id: 10,
-            text: '10'
+            title: ''
           }
-        ]
+        }
+        const end = this.answers[1].answer.substr(12)
+        const index = end.lastIndexOf('-')
+        return {
+          id: parseInt(end.substr(index + 1)),
+          title: end.substr(0, index)
+        }
+      }
+    },
+    methods: {
+      checked (index) {
+        if (this.responses.length) {
+          if (parseInt(this.responses[0].response) === index) {
+            return 'checked'
+          }
+        }
+        return 'unchecked'
+      },
+      onSave (value) {
+        if (this.responses.length) {
+          this.$emit('update-response', [null, value, this.responses[0].id])
+        } else {
+          this.$emit('create-response', [null, value])
+        }
       }
     }
   }
