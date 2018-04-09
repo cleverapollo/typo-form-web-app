19df99e240e38a3cebcea7f2c16d0769470c2ad2
cleverapollo <cleverapollo@gmail.com>
2018-04-09 14:45:21 +0100

    Add Back and cancel buttons

--- src/components/Form/CreateForm.vue
+++ src/components/Form/CreateForm.vue
@@ -6,28 +6,26 @@
           <v-card-text>
             <v-container>
               <h1 class="mb-4">Create Form</h1>
-              <form @submit.prevent="onCreateForm">
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      name="name"
-                      label="Name"
-                      id="name"
-                      v-model="name"
-                      required></v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-btn
-                      class="primary"
-                      :disabled="!formIsValid"
-                      type="submit">Create Form</v-btn>
-                  </v-flex>
-                </v-layout>
-              </form>
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="name"
+                    label="Name"
+                    id="name"
+                    v-model="name"
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
             </v-container>
           </v-card-text>
+          <v-card-actions>
+            <v-btn color="info" @click=onCancel>Cancel</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              class="primary"
+              :disabled="!formIsValid"
+              @click="onCreateForm">Create Form</v-btn>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -85,6 +83,9 @@
           name: this.name
         }
         this.$store.dispatch('createForm', formData)
+        this.$router.push('/applications/' + this.application_id + '/forms')
+      },
+      onCancel () {
         this.$router.push('/applications/' + this.application_id + '/forms')
       }
     },
