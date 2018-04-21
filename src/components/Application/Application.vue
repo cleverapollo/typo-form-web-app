5e2974a56d9c24a3818af62c1bc3b50f3b10448b
cleverapollo <cleverapollo@gmail.com>
2018-04-21 02:57:49 +0200

    update router for auth

---
+++ src/components/Application/Application.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'application' // todo: redirect to first page if getting application fails
+  }
+</script>
+
+<style scoped>
+
+</style>
