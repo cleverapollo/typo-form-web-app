65b6ae53a19f6c5dad1a1f387c3b86733d7528e9
cleverapollo <cleverapollo@gmail.com>
2018-04-26 01:58:50 +0200

    file upload component

--- src/components/Form/Response/components/FileUpload.vue
+++ src/components/Form/Response/components/FileUpload.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-    File Upload
-  </div>
+  <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
 </template>
 
 <script>
+  import vue2Dropzone from 'vue2-dropzone/dist/vue2Dropzone.js' // https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
+  import 'vue2-dropzone/dist/vue2Dropzone.css'
   export default {
-    name: 'fileupload'
+    name: 'fileupload',
+    components: {
+      vueDropzone: vue2Dropzone
+    },
+    data: function () {
+      return {
+        dropzoneOptions: {
+          url: '/file',
+          thumbnailWidth: 150,
+          maxFilesize: 1
+          // headers: { "My-Awesome-Header": "header value" }
+        }
+      }
+    }
   }
 </script>
