4bfbb0c93f370dbf9a146a9360e64d6e75a60902
cleverapollo <cleverapollo@gmail.com>
2018-06-15 11:37:42 +0100

    Fix route

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express()
 const port = process.env.PORT || 8080
 const router = express.Router()
 
-app.use(express.static(`${__dirname}/dist`))
+app.use(express.static(`dist`))
 
 app.engine('.html', require('ejs').renderFile)
 
-app.set('views', `${__dirname}/dist`)
+app.set('views', `dist`)
 
 router.get('/*', (req, res, next) => {
-	res.sendFile(`${__dirname}/dist/index.html`)
+	res.sendFile(`dist/index.html`)
 })
 
 app.use('/', router)
