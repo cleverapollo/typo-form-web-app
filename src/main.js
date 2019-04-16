ca4f3257c299827d38dc012425f8664102a10ded
cleverapollo <cleverapollo@gmail.com>
2019-04-16 14:40:56 +1000

    Internet Explorer 11 Polyfill fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import JsonExcel from 'vue-json-excel'
 import VueQuillEditor from 'vue-quill-editor'
 import axios from 'axios'
-import 'babel-polyfill'
 
 import App from './App'
 import router from './router'
