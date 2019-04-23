a6559e576ea2d09a0db93244abf6e9d30204a988
cleverapollo <cleverapollo@gmail.com>
2019-04-23 18:37:32 +1000

    Modified UI to match other application functions

--- src/components/Note/Notes.vue
+++ src/components/Note/Notes.vue
@@ -1,129 +1,181 @@
 <template>
-    <div>
-        <v-toolbar flat color="white">
-            <v-toolbar-title>Notes</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on }">
-                    <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
-                </template>
-                <v-card>
-                    <v-card-title>
-                        <span class="headline">{{ formTitle }}</span>
-                    </v-card-title>
+  <v-layout row wrap>
 
-                    <v-card-text>
-                        <v-container grid-list-md>
-                            <v-layout wrap>
-                                <v-flex xs12>
-                                    <v-text-field v-model="editedItem.event" label="Event"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12>
-                                    <v-text-field v-model="editedItem.note" label="Note"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12>
-                                    <v-select
-                                            v-model="editedItem.recordable_type"
-                                            :items="types"
-                                            label="Type"
-                                    ></v-select>
-                                </v-flex>
-                                <v-flex xs12>
-                                    <v-select
-                                            v-model="editedItem.recordable_id"
-                                            :items="records(editedItem.recordable_type)"
-                                            label="Record"
-                                            item-text="name"
-                                            item-value="id"
-                                    ></v-select>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                        <span v-if="errorMessage" class="red--text">{{ errorMessage }}</span>
-                    </v-card-text>
+    <v-flex d-flex xs12>
+      <h1 class='headline primary--text py-3'>Notes</h1>
+    </v-flex>
 
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-                        <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
-        </v-toolbar>
-        <v-card>
-            <v-card-title>
-                Nutrition
-                <v-spacer></v-spacer>
-                <v-text-field
-                        v-model="search"
-                        append-icon="search"
-                        label="Search"
-                        single-line
-                        hide-details
-                        :loading="loadingNotes"
-                ></v-text-field>
-            </v-card-title>
-            <v-data-table
-                    :headers="headers"
-                    :items="notes"
-                    class="elevation-1"
-                    :search="search"
-            >
-                <template v-slot:items="props">
-                    <td>{{ props.item.event }}</td>
-                    <td>{{ props.item.note }}</td>
-                    <td>{{ props.item.recordable_type }}</td>
-                    <td>{{ props.item.recordable }}</td>
-                    <td class="justify-center layout px-0">
-                        <v-icon
-                                small
-                                class="mr-2"
-                                @click="editItem(props.item)"
-                        >
-                            edit
-                        </v-icon>
-                        <v-icon
-                                small
-                                @click="deleteItem(props.item)"
-                        >
-                            delete
-                        </v-icon>
-                    </td>
-                </template>
-                <v-alert v-slot:no-results :value="true" color="error" icon="warning">
-                    Your search for "{{ search }}" found no results.
-                </v-alert>
-            </v-data-table>
-        </v-card>
-    </div>
+    <v-flex d-flex xs12>
+      <p>Select an existing note below or <a href='#' @click.stop='dialog = true'>create a new note</a>.</p>
+    </v-flex>
+
+    <v-flex d-flex xs12>
+      <v-card>
+        <v-card-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="notes"
+          :search="search"
+          :loading="loadingNotes"
+          :rows-per-page-items="[25, 50, 100, { text: '$vuetify.dataIterator.rowsPerPageAll', value: -1 }]"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.type }}</td>
+            <td>{{ props.item.description }}</td>
+            <td>{{ props.item.recordable_type }}</td>
+            <td>{{ props.item.recordable }}</td>
+            <td>{{ props.item.updated_at }}</td>
+            <td class="justify-center layout px-0">
+              <v-tooltip bottom>
+                <v-btn icon class='mx-0' @click='editItem(props.item)' slot="activator">
+                  <v-icon color='teal'>edit</v-icon>
+                </v-btn>
+                <span>Edit</span>
+              </v-tooltip>
+              <v-tooltip bottom>
+                <v-btn icon class='mx-0' @click='deleteItem(props.item)' slot="activator">
+                  <v-icon color='pink'>delete</v-icon>
+                </v-btn>
+                <span>Delete</span>
+              </v-tooltip>
+            </td>
+          </template>
+          <v-alert v-slot:no-results :value="true" color="error" icon="warning">
+            Your search for "{{ search }}" found no results.
+          </v-alert>
+        </v-data-table>
+      </v-card>
+    </v-flex>
+
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <div class="title mb-2 mt-2">{{ dialogTitle }}</div>
+        </v-card-title>
+        <v-card-text>
+
+          <v-layout row wrap>
+
+            <v-flex xs12>
+              <v-autocomplete
+                :items="noteTypes"
+                item-value="id"
+                item-text="type"
+                v-model="note.note_type_id"
+                label="Note type"
+              ></v-autocomplete>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-autocomplete
+                :items="recordableTypes"
+                v-model="note.recordable_type"
+                label="Record type"
+              ></v-autocomplete>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-autocomplete
+                v-model="note.recordable_id"
+                :items="records(note.recordable_type)"
+                label="Record"
+                item-text="name"
+                item-value="id"
+              ></v-autocomplete>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-text-field
+                v-model="note.description"
+                label="Description"
+              ></v-text-field>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-textarea
+                v-model="note.note"
+                label="Note"
+                auto-grow
+              ></v-textarea>
+            </v-flex>
+
+          </v-layout>
+
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-layout row py-2>
+            <v-flex xs12 class="text-xs-right">
+              <v-btn 
+                flat 
+                @click.stop="close"
+              >Cancel
+              </v-btn>
+              <v-btn
+                flat
+                class="primary"
+                @click.stop="save"
+              >Save
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-tooltip top>
+      <v-btn slot='activator' fixed dark bottom right fab router class='red' @click.stop='dialog = true'>
+        <v-icon>add</v-icon>
+      </v-btn>
+      <span>Create Note</span>
+    </v-tooltip>
+
+  </v-layout>
 </template>
 <script>
+  import sortBy from 'lodash/sortBy'
   export default {
     data: () => ({
       slug: window.location.hostname.split('.')[0],
       dialog: false,
       search: '',
       headers: [
-        { text: 'Event', value: 'event' },
-        { text: 'Note', value: 'note' },
-        { text: 'Type', value: 'recordable_type' },
+        { text: 'Type', value: 'type' },
+        { text: 'Description', value: 'description' },
+        { text: 'Record Type', value: 'recordable_type' },
         { text: 'Record', value: 'recordable' },
+        { text: 'Updated', value: 'updated_at' },
         { text: 'Actions', value: 'name', sortable: false }
       ],
-      types: [
-        'Form',
-        'Organisation',
-        'User'
+      recordableTypes: [
+        'User',
+        'Organisation'
+      ],
+      noteTypes: [
+        { id: 1, type: 'Phone' },
+        { id: 2, type: 'Email' },
+        { id: 3, type: 'Other' }
       ],
       editedIndex: -1,
-      editedItem: {
-        event: '',
+      note: {
+        note_type_id: '',
+        description: '',
         note: '',
         recordable_type: '',
         recordable_id: ''
       },
-      defaultItem: {
-        event: '',
+      noteTemplate: {
+        note_type_id: '',
+        description: '',
         note: '',
         recordable_type: '',
         recordable_id: ''
@@ -137,12 +189,14 @@
         notes.forEach((record) => {
           const list = this.records(record.recordable_type)
           const element = list.find(value => value.id === record.recordable_id)
+          const recordType = record.note_type_id ? this.noteTypes.find(noteType => { return noteType.id === record.note_type_id }) : null
+          record.type = recordType ? recordType.type : ''
           record.recordable = element ? element.name : ''
         })
         return notes
       },
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      dialogTitle () {
+        return this.editedIndex === -1 ? 'Create Note' : 'Edit Note'
       }
     },
     watch: {
@@ -153,7 +207,7 @@
     methods: {
       editItem (item) {
         this.editedIndex = this.notes.indexOf(item)
-        this.editedItem = Object.assign({}, item)
+        this.note = Object.assign({}, item)
         this.dialog = true
       },
       deleteItem (item) {
@@ -166,63 +220,53 @@
         this.dialog = false
         this.errorMessage = ''
         setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
+          this.note = Object.assign({}, this.noteTemplate)
           this.editedIndex = -1
         }, 300)
       },
       save () {
-        if (!this.editedItem.event || !this.editedItem.note) {
-          this.errorMessage = 'Event and Note are required.'
-          return
-        }
         if (this.editedIndex > -1) {
           const updateObj = {
-            id: this.editedItem.id,
+            id: this.note.id,
             slug: this.slug,
-            event: this.editedItem.event,
-            note: this.editedItem.note,
-            recordable_type: this.editedItem.recordable_type,
-            recordable_id: this.editedItem.recordable_id
+            note_type_id: this.note.event,
+            description: this.note.description,
+            note: this.note.note,
+            recordable_type: this.note.recordable_type,
+            recordable_id: this.note.recordable_id
           }
           this.$store.dispatch('updateNote', updateObj)
         } else {
           this.$store.dispatch('createNote', {
             slug: this.slug,
-            event: this.editedItem.event,
-            note: this.editedItem.note,
-            recordable_type: this.editedItem.recordable_type,
-            recordable_id: this.editedItem.recordable_id
+            note_type_id: this.note.note_type_id,
+            description: this.note.description,
+            note: this.note.note,
+            recordable_type: this.note.recordable_type,
+            recordable_id: this.note.recordable_id
           })
         }
         this.close()
       },
       records (type) {
         let records = []
-        if (type === 'Form') {
-          records = this.$store.getters.loadedAllForms(this.slug)
-          records.forEach((record) => {
-            record.name = record.form_template.name
-          })
-        }
-        if (type === 'Organisation') {
-          records = this.$store.getters.loadedOrganisations(this.slug)
-        }
         if (type === 'User') {
           records = this.$store.getters.loadedUsers(this.slug)
           records.forEach((record) => {
-            record.name = record.first_name + ' ' + record.last_name
+            record.name = record.first_name + ' ' + record.last_name + ' (' + record.email + ')'
           })
         }
-        return records
+        if (type === 'Organisation') {
+          records = sortBy(this.$store.getters.loadedOrganisations(this.slug), organisation => { return organisation.name })
+        }
+        return sortBy(records, record => { return record.name })
       }
     },
     created () {
       this.loadingNotes = true
       Promise.all([
-        this.$store.dispatch('loadOrganisations', this.slug),
-        this.$store.dispatch('loadFormTemplates', this.slug),
-        this.$store.dispatch('loadAllForms', this.slug),
-        this.$store.dispatch('loadUsers', this.slug)
+        this.$store.dispatch('loadUsers', this.slug),
+        this.$store.dispatch('loadOrganisations', this.slug)
       ])
         .then(() => {
           this.$store.dispatch('loadNotes', this.slug)
